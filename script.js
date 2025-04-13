0f3190e8259d63c1d26644c7ca08a4f4084e2d34
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2025-04-13 11:46:24 +1000

    add js function

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 //alert('test');
 const inputField = document.getElementById('inputField');
 const outputField = document.getElementById('outputField');
+const degug = document.getElementById('debug');
 
-function addTask(){
-    
+function addTask() {
+    if (inputField.value === "") {
+        alert("You must add the task");
+    } else {
+        // degug.innerHTML =inputField.value;
+        let li = document.createElement('li');
+        // console.log("test");
+        li.innerHTML = inputField.value;
+        outputField.appendChild(li);
+    }
 }
