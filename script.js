d23cbe4b0fefec05263c8c6c9f7650aaff91cfed
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2025-04-13 12:01:57 +1000

    add delete button in js file

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ function addTask() {
     if (inputField.value === "") {
         alert("You must add the task");
     } else {
-        // degug.innerHTML =inputField.value;
-        let li = document.createElement('li');
-        // console.log("test");
+        // Create a new list item and add it to the output field       
+        let li = document.createElement('li');        
         li.innerHTML = inputField.value;
         outputField.appendChild(li);
+        // Create a delete button for the new list item
+        let deleteButton = document.createElement('button');    
+        deleteButton.innerHTML = "Delete";
+        li.appendChild(deleteButton);
     }
+    inputField.value = ""; // Clear the input field after adding the task
+    inputField.focus(); // Set focus back to the input field
 }
